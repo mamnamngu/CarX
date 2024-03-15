0d98e98d61b01393a79c6c4751f3a5f02f2bdb2d
mamnamngu <asouth93new@gmail.com>
2024-03-15 08:35:43 +0700

    quick patches

--- src/main/java/com/swd/carx/controller/tournamentManagement/CarProgressController.java
+++ src/main/java/com/swd/carx/controller/tournamentManagement/CarProgressController.java
@@ -17,22 +17,37 @@ public class CarProgressController {
 	public ResponseEntity<CarProgressDTO> updateProgress(@RequestBody CarProgressDTO progress){
 		try {		
 			System.out.println(progress);
-			writeToDatabase(progress.getCarRaceId(), progress.getProgress());
+			writeToDatabase(progress);
+//			TestriteToDatabase();
 			return ResponseEntity.ok(progress);
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).header("message", "Failed to add new progress for carInRace: " + progress.getCarRaceId()).build();
 		}
 	}
 	
-	 private void writeToDatabase(long carRaceId, int progress) {
-	        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("car-progress");
-	        DatabaseReference ref = dbRef.child(String.valueOf(carRaceId));
-	        ref.setValue(progress, (databaseError, databaseReference) -> {
-	            if (databaseError != null) {
-	                System.out.println("Data could not be saved " + databaseError.getMessage());
-	            } else {
-	                System.out.println("Data saved successfully.");
-	            }
-	        });
-	    }
+	 private void writeToDatabase(CarProgressDTO progress) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("car-progress");
+        DatabaseReference ref = dbRef.child(String.valueOf(progress.getCarRaceId()));
+        ref.setValue(progress, (databaseError, databaseReference) -> {
+            if (databaseError != null) {
+                System.out.println("Data could not be saved " + databaseError.getMessage());
+            } else {
+                System.out.println("Data saved successfully.");
+            }
+        });
+    }
+	 
+	 private void TestriteToDatabase() {
+		long testCarRaceId = 4;
+		long testProgress = 80;
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("car-progress");
+        DatabaseReference ref = dbRef.child(String.valueOf(testCarRaceId));
+        ref.setValue(testProgress, (databaseError, databaseReference) -> {
+            if (databaseError != null) {
+                System.out.println("Data could not be saved " + databaseError.getMessage());
+            } else {
+                System.out.println("Data saved successfully.");
+            }
+        });
+    }
 }
